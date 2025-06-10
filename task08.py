ff0fe4d3209e4e520bbac76324d1475652b87f11
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-10 23:47:34 +0300

    misol08

---
+++ task08.py
@@ -0,0 +1,19 @@
+pasword = 0000
+s = 0
+while s < 3:
+    p = int(input("parol >> "))
+    s += 1
+    if p == pasword:
+        print("Togri")
+        break
+
+    else:
+        print("xato")
+
+else:
+    print("bloksiz")
+
+
+
+
+    
