a4ef87c3e925a3d424023d53ec8bd7b9b9cdc385
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-10 23:27:34 +0300

    misol04

---
+++ task04.py
@@ -0,0 +1,12 @@
+from random import randint
+
+r = randint(1,20)
+
+while True:
+    son  = int(input())
+    if son == r:
+        print("topdiz")
+        break
+
+    else:
+       print("topolmadiz")
